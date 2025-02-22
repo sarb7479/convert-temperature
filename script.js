359428bfe4b192ee7675385ee190739425aad78b
sarb7479 <128984563+sarb7479@users.noreply.github.com>
2025-02-22 11:57:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,11 +10,23 @@ function convertTemperature() {
     }
     if (temperatureType === "Celsius") {
         result = (temperature - 32) * 5 / 9;
-        document.getElementById("resultDisplay").textContent = result.toFixed(2) + " °C";
+        document.getElementById("temperatureResultDisplay").textContent = result.toFixed(2) + " °C";
     } else if (temperatureType === "Fahrenheit") {
         result = (temperature * 9 / 5) + 32;
-        document.getElementById("resultDisplay").textContent = result.toFixed(2) + " °F";
+        document.getElementById("temperatureResultDisplay").textContent = result.toFixed(2) + " °F";
     }
     
     return result;
+}
+
+function overallGrade() {
+    var assignmentGrade = parseFloat(document.getElementById("assignmentGrade").value);
+    var midtermGrade = parseFloat(document.getElementById("midtermGrade").value);
+    var finalProjectGrade = parseFloat(document.getElementById("finalProjectGrade").value);
+    var participationGrade = parseFloat(document.getElementById("participationGrade").value);
+    var largest = Math.max(assignmentGrade, midtermGrade, finalProjectGrade, participationGrade);
+
+    var overallGrade = ((0.30 * assignmentGrade) + (0.25 * midtermGrade) + (0.30 * finalProjectGrade) + (0.10 * participationGrade) + (0.05 * largest));
+    document.getElementById("gradeResultDisplay").textContent = overallGrade.toFixed(2);
+    return overallGrade
 }
